f2825e5a03bb1e1467cd5b09b078948ee0bce1f1
Mehmet AK <114513430+mehmetak7@users.noreply.github.com>
2023-07-24 15:49:45 +0300

    Event Model Added
    Model sınıfı ekleme

---
+++ Application/Application/Models/Event.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Application.Models
+{
+
+    public class EventRequest
+    {
+        public string EventName { get; set; }
+        public string EventType { get; set; }
+        public DateTime MeatingDateTime { get; set; }
+        public string MeetingNotes { get; set; }
+    }
+    public class EventResponse
+    {
+       // public bool IsSuccessfull { get; set; }
+       // public string Message { get; set; }
+        public string EventName { get; set; }
+        public string EventType { get; set; }
+        public DateTime MeatingDateTime { get; set; }
+        public string MeetingNotes { get; set; }
+
+    }
+
+
+}
