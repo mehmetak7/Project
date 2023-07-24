ec783e2b497dc1dbbacbb37f81148e15257b916d
İrem Özben <56583531+iremozbn@users.noreply.github.com>
2023-07-24 14:03:48 +0300

    Add files via upload

---
+++ Application/Application/Models/Meeting.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Application.Models
+{
+
+    public class MeetingRequest
+    {
+        public string TeamName { get; set; }
+        public string MeetingName { get; set; }
+        public DateTime MeetingDate { get; set; }
+        public DateTime MeetingTime { get; set; }
+        public string MeetingContext { get; set; }
+        public string MeetingContent { get; set; }
+    }
+    public class MeetingResponse
+    {
+        public bool IsMeeting { get; set; }
+        public string TeamName { get; set; }
+        public string MeetingName { get; set; }
+        public DateTime MeetingDate { get; set; }
+        public DateTime MeetingTime { get; set; }
+        public string MeetingContext { get; set; }
+        public string MeetingContent { get; set; }
+    }
+
+
+}
