7feff6baefaf7f45a90fad1474a95076fc4b69ea
İrem Özben <56583531+iremozbn@users.noreply.github.com>
2023-07-25 15:13:34 +0300

    Update EventController.cs

--- Application/Application/Controllers/EventController.cs
+++ Application/Application/Controllers/EventController.cs
@@ -42,7 +42,7 @@ namespace Application.Controllers
                     .Where(action => action != null)
                     .ToList();
 
-                return Ok(events.OrderBy(e => e.EventDateTime).ToList());
+                return Ok(events.OrderBy(e => e.MeatingDateTime).ToList());
 
             }
             catch (Exception ex)
