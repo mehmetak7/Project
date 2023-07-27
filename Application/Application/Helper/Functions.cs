c1252d1e05bc193bbddab117355243b47c18a39a
İrem Özben <56583531+iremozbn@users.noreply.github.com>
2023-07-27 14:56:39 +0300

    Update Functions.cs
    son halini yükledim

--- Application/Application/Helper/Functions.cs
+++ Application/Application/Helper/Functions.cs
@@ -8,110 +8,32 @@ namespace Application.Helper
 {
     public class Functions
     {
-    public string LoadLoginsFromFile(string filePath, int InputSicilNo)
-    {
+        public string LoadLoginsFromFile(string filePath, int InputSicilNo)
+        {
             var login = string.Empty;
             try
-        {
-            string[] lines = System.IO.File.ReadAllLines(filePath);
-
-            // LINQ sorgusu ile gerekli satırı buluyoruz.
-             login = lines.FirstOrDefault(line =>
             {
-                string[] parts = line.Split(';');
-                return int.TryParse(parts[0], out int sicilNo) && sicilNo == InputSicilNo;
-            });
+                string[] lines = System.IO.File.ReadAllLines(filePath);
 
-            return login;
-        }
-        catch
-        {
-            // Hata durumunda boş login = null döner...
-            return login;
-        }
-    }
-
-        /*
-        public bool LoadBDay(string filepath, int InputSicilNo)
-        {
-            var BDay = string.Empty;
-            bool check = false;
-            
-            string[] lines = System.IO.File.ReadAllLines(filepath);
-
-        
-            var Bday = lines.FirstOrDefault(line =>
-            {
-                string[] parts = line.Split(";");
-                string todayDate = DateTime.Now.ToString();
-                string[] today = todayDate.Split(".");
-                string date = parts[parts.Length - 1];
-                string[] dateBD = date.Split("/");
-                if (string.Compare(dateBD[0], today[0]) == string.Compare(today[1], dateBD[1]))
+                // LINQ sorgusu ile gerekli satırı buluyoruz.
+                login = lines.FirstOrDefault(line =>
                 {
-                        check = true;
-                }
-                else
-                    check = false;
-
-                return (int.TryParse(parts[0], out int sicilNo) && sicilNo == InputSicilNo) && (string.Compare(dateBD[0], today[0]) == string.Compare(today[1], dateBD[1]));
-            });
-            
-
-            return check;
-            
-
-        }
-        //Burada hata var date'ler tutuyorsa da liste dönüyor her türlü false dönüyor sicilNo kontrolü yani sadece
-        //o kişinin doğuum günü mü değil mi o yapılmıyor... asagidaki fonksiyon sadece login de girilen kişinin doğum günü mü
-        //değil mi o yapılıyor...
-        */
-        /*
-        public bool IsBirthdayToday(string filepath, int InputSicilNo)
-        {
-            string todayDate = DateTime.Now.ToString("dd/MM");
-            bool isBirthdayToday = false;
-            int? test = InputSicilNo;
-
-            if (test != null)
-            {
-                string[] lines = System.IO.File.ReadAllLines(filepath);
-
-                var birthdayLine = lines.FirstOrDefault(line =>
-                {
-                    string[] parts = line.Split(";");
-
-                    int sicilNo;
-                    string date = parts[parts.Length - 1];
-
-                    if (int.TryParse(parts[0], out sicilNo) && sicilNo == InputSicilNo)
-                    {
-                        string[] dateBD = date.Split("/");
-                        string birthday = dateBD[0] + "." + dateBD[1];
-
-                        if (birthday == todayDate)
-                        {
-                            isBirthdayToday = true;
-                            return isBirthdayToday;
-                        }
-                    }
-
-                    return false;
+                    string[] parts = line.Split(';');
+                    return int.TryParse(parts[0], out int sicilNo) && sicilNo == InputSicilNo;
                 });
-            }
-            else
-            {
-                return false;
-            }
-            
-            return isBirthdayToday;     
-        }
-        */
 
+                return login;
+            }
+            catch
+            {
+                // Hata durumunda boş login = null döner...
+                return login;
+            }
+        }
 
         public bool IsBirthdayToday(string filepath, int InputSicilNo)
         {
-            string todayDate = DateTime.Now.ToString("MM.dd");
+            string todayDate = DateTime.Now.ToString("dd.MM");
 
             if (InputSicilNo <= 0) // Sicil numarası geçerli bir değer olmalıdır.
                 return false;
@@ -128,7 +50,7 @@ namespace Application.Helper
                 if (int.TryParse(parts[0], out sicilNo) && sicilNo == InputSicilNo)
                 {
                     string[] dateBD = date.Split("/");
-                    string birthday = dateBD[1] + "." + dateBD[0];
+                    string birthday = dateBD[0] + "." + dateBD[1];
 
                     return birthday == todayDate;
                 }
@@ -138,8 +60,5 @@ namespace Application.Helper
 
             return birthdayLine != null;
         }
-
-
     }
 }
-
