cf7be585a72e18dda246d8390db049c5ec5cfcaa
Mehmet AK <114513430+mehmetak7@users.noreply.github.com>
2023-08-01 14:57:52 +0300

    Update BDay.cs
    Doğum günü ekranı için istenilen yaklaşan doğum günler bilgilerini response ederken GünSayacı ekledim

--- Application/Application/Models/BDay.cs
+++ Application/Application/Models/BDay.cs
@@ -5,11 +5,12 @@ using System.Threading.Tasks;
 
 namespace Application.Models
 {
-    public class BDayResponse
+   public class BDayResponse
     {
         public string Name { get; set; }
         public string Surname { get; set; }
         public string BirthDay { get; set; }
+        public int CounterOfDay { get; set; }
     }
 }
 
