11f0dffe8c707085c83f01ed942b84359da766ef
İrem Özben <56583531+iremozbn@users.noreply.github.com>
2023-08-01 17:13:32 +0300

    Search model Added

---
+++ Application/Application/Models/Search.cs
@@ -0,0 +1,18 @@
+﻿namespace Application.Models
+{
+    public class Search
+    {
+        public class SearchRequest
+        {
+            public int Id { get; set; }
+            public string TeamName { get; set; }
+            public string MeetingName { get; set; }
+        }
+        public class SearchEventRequest
+        {
+            public int Id { get; set; }
+            public string EventName { get; set; }
+            public string EventType { get; set; }
+        }
+    }
+}
